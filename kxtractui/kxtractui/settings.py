663603936c7a48123af49dffee34aa0d4fa58254
Frank Kelly <kellyfj@gmail.com>
2020-01-20 13:34:50 -0500

    Rename folder

--- kxtractui/kxtractui/settings.py
+++ kxtractui/kxtractui/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'podcasts.apps.PodcastsConfig',
+    'ui.apps.UIConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- kxtractui/kxtractui/urls.py
+++ kxtractui/kxtractui/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('podcasts/', include('podcasts.urls')),
+    path('ui/', include('ui.urls')),
     path('admin/', admin.site.urls),
 ]
 
